4624cde1bd5e447c0cc8736e914a517301006038
ACostaCaz <mankydev@gmail.com>
2022-05-08 20:29:05 +0100

    Added profile creation

--- src/app/interfaces/business-profile.interface.ts
+++ src/app/interfaces/business-profile.interface.ts
@@ -1,8 +1,12 @@
 export interface businessProfile {
     id: string;
     name: string;
-    ogCost: string;
-    discounted: string;
-    description: number;
-    user_id: string;
+    address: string;
+    monday: string;
+    tuesday: string;
+    wednesday: string;
+    thursday: string;
+    friday: string;
+    saturday: string;
+    sunday: string;
 }
--- src/app/profile-management/profile-management.component.ts
+++ src/app/profile-management/profile-management.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-
+import { profileService } from '../services/profile.service';
+import {AuthService} from '../services/auth.service';
 @Component({
   selector: 'app-profile-management',
   templateUrl: './profile-management.component.html',
   styleUrls: ['./profile-management.component.css']
 })
 export class ProfileManagementComponent implements OnInit {
-
-  constructor() { }
+  name!: string;
+  address!: string;
+  category!: string;
+  monday!: string;
+  tuesday!: string;
+  wednesday!: string;
+  thursday!: string;
+  friday!: string;
+  saturday!: string;
+  sunday!: string;
+  constructor(private profileService: profileService, private AuthService: AuthService) { }
 
   ngOnInit(): void {
+  }
+  createProfile() {
+    this.profileService.create({
+      id: this.AuthService.getUid(),
+      address: this.address,
+      name: this.name,
+      monday: this.monday,
+      tuesday: this.tuesday,
+      wednesday: this.wednesday,
+      thursday: this.thursday,
+      friday: this.friday,
+      saturday: this.saturday,
+      sunday: this.sunday
+    })
   }
 
 }
